33aa365e2dbaaa9b34b10f9ffdb19e435d8e5445
agentzh (章亦春) <agentzh@gmail.com>
2010-04-10 01:46:05 +0800

    added the test suite.

---
+++ util/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# this file is mostly meant to be used by the author himself.
+
+#ragel -I src -G2 src/ngx_http_memc_response.rl
+
+if [ $? != 0 ]; then
+    echo 'Failed to generate the memcached response parser.' 1>&2
+    exit 1;
+fi
+
+root=`pwd`
+cd ~/work
+version=$1
+opts=$2
+home=~
+lwp-mirror "http://sysoev.ru/nginx/nginx-$version.tar.gz" nginx-$version.tar.gz
+tar -xzvf nginx-$version.tar.gz
+cd nginx-$version/
+if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$root/util/build.sh" -nt Makefile ]]; then
+    ./configure --prefix=/opt/nginx \
+          --with-http_addition_module \
+          --add-module=$root $opts \
+          --add-module=$root/../echo-nginx-module \
+          --add-module=$root/../memc-nginx-module \
+          --with-debug
+          #--add-module=$home/work/nginx/nginx_upstream_hash-0.3 \
+  #--without-http_ssi_module  # we cannot disable ssi because echo_location_async depends on it (i dunno why?!)
+
+fi
+if [ -f /opt/nginx/sbin/nginx ]; then
+    rm -f /opt/nginx/sbin/nginx
+fi
+if [ -f /opt/nginx/logs/nginx.pid ]; then
+    kill `cat /opt/nginx/logs/nginx.pid`
+fi
+make -j3
+make install
+
